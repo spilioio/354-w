103fb2a3721ca95028e60dbfc2a0c7f57f1dfe0c
fbobier <frederique.bobier@gmail.com>
2015-02-01 21:23:55 -0500

    User and UserTest classes should be okay.

--- Increment1/src/tests/UserTest.java
+++ Increment1/src/tests/UserTest.java
@@ -6,11 +6,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.util.ArrayList;
 
 import org.junit.Test;
 
-import core.Task;
 import core.User;
 
 /**
@@ -19,15 +17,13 @@ import core.User;
  * @version 1.0
  */
 public class UserTest {
-	private User myUser;
+	private static User myUser = new User("f_bobier", "qwerty", "Fred", "Bob");
 
 	@Test
 	public void testUserConstructor() {
-
 		// Create a user: userName (PK - unique), userPwd, fName, lName
 		// The password shouldn't be stored
-		myUser = new User("f_bobier", "qwerty", "Fred", "Bob");
-
+		
 		// Check to see that the user has been added to the database
 		Connection conn = null;
 		try {
@@ -35,7 +31,7 @@ public class UserTest {
 			conn = DriverManager.getConnection("jdbc:sqlite:COMP354");
 
 			Statement stmt = conn.createStatement();
-			ResultSet rs = stmt.executeQuery("SELECT * FROM users WHERE user_id = f_bobier AND user_pwd = qwerty;");
+			ResultSet rs = stmt.executeQuery("SELECT * FROM users WHERE user_id = 'f_bobier' AND user_pwd = 'qwerty';");
 
 			while (rs.next()) {
 				assertEquals(rs.getString("user_id"), myUser.getName());
@@ -62,7 +58,7 @@ public class UserTest {
 			conn = DriverManager.getConnection("jdbc:sqlite:COMP354");
 
 			Statement stmt = conn.createStatement();
-			ResultSet rs = stmt.executeQuery("SELECT * FROM users WHERE user_id = f_bobier;");
+			ResultSet rs = stmt.executeQuery("SELECT * FROM users WHERE user_id = 'f_bobier';");
 
 			while (rs.next()) {
 				assertEquals(rs.getString("user_id"), myUser.getName());
@@ -88,7 +84,7 @@ public class UserTest {
 			conn = DriverManager.getConnection("jdbc:sqlite:COMP354");
 
 			Statement stmt = conn.createStatement();
-			ResultSet rs = stmt.executeQuery("SELECT * FROM users WHERE user_id = f_bobier;");
+			ResultSet rs = stmt.executeQuery("SELECT * FROM users WHERE user_id = 'f_bobier';");
 
 			assertEquals(rs.getRow(), 0);
 
