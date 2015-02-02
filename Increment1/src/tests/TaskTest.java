2211779db41e3ced0c666b367e2ff8ffebaa0941
fbobier <frederique.bobier@gmail.com>
2015-02-01 19:42:41 -0500

    Modified TaskTest; might need some fixing with the queries.

--- Increment1/src/tests/TaskTest.java
+++ Increment1/src/tests/TaskTest.java
@@ -2,54 +2,111 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 import core.Task;
 
+/**
+ * 
+ * @author Frédérique Bobier - 1952765
+ * @version 1.0
+ */
 public class TaskTest {
 
-	private Task t1, t2;
+	private Task t1;
+	private ArrayList<Integer> myTasks = new ArrayList<Integer>();
 
 	@Test
-	public void testInit() {
-		// project id, task id, user id, earliest start, duration, label
-		t1 = new Task(3, 1, 2, 0, 1, "t1");
-		t2 = new Task(3, 2, 2, 0, 4, "t2");
+	public void testTaskConstructorOne() {
+		myTasks.add(1);
+		//name, description, duration, project_id, owner_id, pre-reqs
+		t1 = new Task("3rd Task", "Complete this after T1", 4, 1, "b_jenkins", myTasks);
+		
+		// Check to see that both tasks has been added to the database
+		Connection conn = null;
+		try {
+			Class.forName("org.sqlite.JDBC");
+			conn = DriverManager.getConnection("jdbc:sqlite:COMP354");
+
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT tasks.task_id, precedence.pre_req FROM tasks JOIN precedence ON tasks.task_id = precedence.task_id;");
+
+			int i = 0;
+			while (rs.next()) {
+				assertEquals(rs.getString("tasks.task_id"), t1.getTaskId());
+				assertEquals(rs.getString("precedence.pre_req"), t1.getPrereq().get(i));
+				i++;
+			}
+			
+			rs.close();
+			stmt.close();
+			conn.close();
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
 	}
 	
 	@Test
-	public void testgetProjectId() {
-		assertEquals(t1.getProjectId(), "3");
-		assertEquals(t2.getProjectId(), "3");
+	public void testEditTask() {
+		t1.setDiscription("Complete this after T1 and T2.");
+		t1.addPrereq(2);
+		t1.finishedTask();
+		
+		// Check to see that both tasks has been added to the database
+		Connection conn = null;
+		try {
+			Class.forName("org.sqlite.JDBC");
+			conn = DriverManager.getConnection("jdbc:sqlite:COMP354");
+
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT tasks.task_id task.is_done, precedence.pre_req FROM tasks JOIN precedence ON tasks.task_id = precedence.task_id;");
+
+			int i = 0;
+			while (rs.next()) {
+				assertEquals(rs.getString("tasks.task_id"), t1.getTaskId());
+				assertEquals(rs.getString("task.is_done"), t1.idDone());
+				assertEquals(rs.getString("precedence.pre_req"), t1.getPrereq().get(i));
+				i++;
+			}
+			
+			rs.close();
+			stmt.close();
+			conn.close();
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
 	}
 	
 	@Test
-	public void testgetTaskId() {
-		assertEquals(t1.getTaskId(), "1");
-		assertEquals(t2.getTaskId(), "2");
-	}
-	
-	@Test
-	public void testgetUserId() {
-		assertEquals(t1.getUserId(), "1");
-		assertEquals(t2.getUserId(), "2");
-	}
-	
-	@Test
-	public void testgetEarliestStart() {
-		assertEquals(t1.getEarliestStart(), "0");
-		assertEquals(t2.getEarliestStart(), "0");
-	}
-	
-	@Test
-	public void testgetDuration() {
-		assertEquals(t1.getDuration(), "1");
-		assertEquals(t2.getDuration(), "4");
-	}
-	
-	@Test
-	public void testgetName() {
-		assertEquals(t1.getName(), "t1");
-		assertEquals(t2.getName(), "t2");
+	public void testDeleteTask() {
+		t1.deleteTask();
+		
+		// Check to see that both tasks has been added to the database
+		Connection conn = null;
+		try {
+			Class.forName("org.sqlite.JDBC");
+			conn = DriverManager.getConnection("jdbc:sqlite:COMP354");
+
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM tasks WHERE name = '3rd Task';");
+
+			//The task will not be able to be deleted if it's a pre-requisit for another task.
+			assertEquals(rs.getRow(), 0);
+			
+			rs.close();
+			stmt.close();
+			conn.close();
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
 	}
 }
